22bcee67b11e99dc8c374e3b32da4e6a86c2fffd
youngestage <youngleslie.ik@gmail.com>
2025-01-27 06:10:09 -0800

    mumumumu

--- src/pages/VendorDashboard.tsx
+++ src/pages/VendorDashboard.tsx
@@ -92,7 +92,7 @@ const VendorDashboard = () => {
         </aside>
 
         {/* Main Content */}
-        <main className="flex-1 p-6 pt-10">
+        <main className="flex-1 p-6 pt-50">
           <Routes>
             <Route path="products" element={<Products />} />
             <Route path="*" element={<Outlet />} />
